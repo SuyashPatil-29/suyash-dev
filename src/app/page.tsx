ee212761a322ba9a45f332e2f1fd3c251d1e78f3
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2024-01-18 22:35:08 +0530

    Added icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Github, Linkedin, Twitter } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
@@ -5,17 +8,31 @@ export default function Home() {
         Oh, hello there 👋
       </div>
       <div className="text-[rgb(177,177,177)] dark:text-black leading-7">
-        <p>👨‍🎓 I’m <span className="underline underline-offset-2">Suyash Patil</span>, a 20 year-old student.</p>
+        <p>
+          👨‍🎓 I’m{" "}
+          <span className="underline underline-offset-2">Suyash Patil</span>, a
+          20 year-old student.
+        </p>
         <p>
           👨‍💻 A self taught programmer, footballer and a lover of all things
           tech.
         </p>
-        <p>
-          ⚒️ I mainly work with Java, TypeScript and Rust on a daily basis.
-        </p>
+        <p>⚒️ I mainly work with Java, TypeScript and Rust on a daily basis.</p>
         <p>🏡 Currently living in the beautiful city of Bengaluru in India.</p>
 
-        <h2 className=" text-xl md:mt-16 mt-10 leading-7">
+        <div className="flex mt-6 gap-4">
+          <Link href="https://github.com/SuyashPatil-29">
+            <Github />
+          </Link>
+          <Link href="https://twitter.com/_suyashpatil">
+            <Twitter />{" "}
+          </Link>
+          <Link href="https://www.linkedin.com/in/suyash-patil-1921b0202/">
+            <Linkedin />
+          </Link>
+        </div>
+
+        <h2 className=" text-xl mt-6 leading-7">
           This blog will serve as a home for all my thoughts, notes and
           experiences, ranging from programming to math, machine learning, web
           development and more. Scroll down and check ‘em out!
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,25 @@
-import MobileNavbar from "./MobileNavbar"
-import NavItem from "./NavItem"
+import MobileNavbar from "./MobileNavbar";
+import NavItem from "./NavItem";
 
-
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
     <div className="flex items-center justify-between mt-8 mx-auto w-full max-w-screen-xl px-4 md:px-20 md:mb-20 mb-14">
-        <div>
-            <NavItem href="/" className=' text-3xl text-white font-bold'>Suyash Patil</NavItem>
-        </div>
-        <div className='md:flex hidden items-center justify-between gap-6'>
-            <NavItem href="/archive" >Archive</NavItem>
-            <NavItem href="/projects" >Projects</NavItem>
-            <NavItem href="/categories" >Categories</NavItem>
-            <NavItem href="/search" >Search</NavItem>
-        </div>
-        <MobileNavbar />
+      <div>
+        <NavItem href="/" className=" text-3xl text-white font-bold">
+          Suyash Patil
+        </NavItem>
+      </div>
+      <div className="md:flex hidden items-center justify-between gap-6">
+        <NavItem href="/archive">Archive</NavItem>
+        <NavItem href="/projects">Projects</NavItem>
+        <NavItem href="/categories">Categories</NavItem>
+        <NavItem href="/search">Search</NavItem>
+      </div>
+      <MobileNavbar />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
